29833b97fa4daaa124e9f7bd0b0a1a1640042d06
Macmini3 <systems@cogzidel.com>
2018-02-02 20:10:38 +0530

    make debug true for log

--- babybuddy/settings/base.py
+++ babybuddy/settings/base.py
@@ -20,7 +20,7 @@ load_dotenv(find_dotenv())
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 SECRET_KEY = os.environ.get('SECRET_KEY', None)
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', True)
 
 
 # Applications
